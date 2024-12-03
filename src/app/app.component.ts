12b58cd814a23af7b3b17e950f3e99f90dabd8c0
godchisa254 <Matias.254@outlook.com>
2024-12-03 19:58:53 -0300

    chore: adds flowbite package

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { OnInit } from '@angular/core';
+import { initFlowbite } from 'flowbite';
 
 @Component({
   selector: 'app-root',
@@ -8,7 +10,11 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'Portfolio_Front';
   profilePicPath = 'image.jpg';
+
+  ngOnInit(){
+    initFlowbite();
+  }
 }
